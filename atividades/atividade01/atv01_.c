23035bd9ca9c32a30231a80948ab700038aa73b9
carlosdanielb <164261112+carlosdanielb@users.noreply.github.com>
2024-04-03 20:55:17 -0300

    atv01_.c

--- atividades/atividade01/atv01_.c
+++ atividades/atividade01/atv01_.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+
+int main(void) {
+
+int x = 10;
+printf("O teste trata-se da pontuação de um jogador de sinuca.\n");
+  printf("o resultado é de %d\n", x);
+  if (x >= 0 && x <= 9)
+  printf("Insulficiente");
+else
+if (x >= 10 && x <= 39)
+  printf("Regular");
+  else
+    if (x >= 40 && x <= 69)
+  printf("Bom");
+  else
+  if (x >= 70 && x <= 89)
+    printf("Otimo");
+  else
+    if (x >= 90 && x <= 100)
+  printf("Excelente");
+return 0;
+}
